bedf96dd01cebb3ac300954f40cc8d0d691bd62c
Jomilto <joselobatof@gmail.com>
2023-01-12 20:18:06 -0600

    feat (tokens): Validate token and scope

--- auth/index.js
+++ auth/index.js
@@ -1,9 +1,40 @@
 const jwt = require('jsonwebtoken');
+const config = require("../config");
+const { secret } = config.JWT;
 
 function sign(data) {
-  return jwt.sign(data, 'secretPassword')
+  return jwt.sign(data, secret)
+}
+
+function verify(token) {
+  return jwt.verify(token, secret)
+}
+
+const check = {
+  own: function (req, owner) {
+    const decodedToken = decodeHeader(req);
+    if(decodedToken.id !== owner) throw new Error("Missing permitions")
+  }
+}
+
+function getToken (auth) {
+  if(!auth) throw new Error('Missing token');
+  if(!auth.includes("Bearer ")) throw new Error("Token not identified");
+  let token = auth.replace("Bearer ", "");
+
+  return token;
+}
+
+function decodeHeader(req) {
+  const { authorization } = req.headers;
+  const token = getToken(authorization);
+  const decoded = verify(token);
+
+  req.user = decoded;
+  return decoded;
 }
 
 module.exports = {
   sign,
+  check,
 };
--- config.js
+++ config.js
@@ -1,5 +1,8 @@
 module.exports = {
   API: {
     PORT: process.env.API_PORT || 3000,
+  },
+  JWT: {
+    secret: process.env.SECRECT || "secretPassword"
   }
 }
