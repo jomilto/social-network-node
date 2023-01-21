6da3fca84660027bb98c093aef5b2a1b9d6f1e19
Jomilto <joselobatof@gmail.com>
2023-01-21 14:37:59 -0600

    feat (db): Mysql connection

--- config.js
+++ config.js
@@ -4,5 +4,12 @@ module.exports = {
   },
   JWT: {
     secret: process.env.SECRECT || "secretPassword"
+  },
+  MYSQL: {
+    host: process.env.MYSQL_HOST || "localhost",
+    port: process.env.MYSQL_PORT || "6033",
+    user: process.env.MYSQL_USER || "db_user",
+    password: process.env.MYSQL_PASSWORD || "db_user_pass",
+    db: process.env.MYSQL_DB || "testing",
   }
 }
